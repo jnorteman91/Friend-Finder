e5706516035352135dab63ee9c21302b24006434
Jd0h Norteman <jnorteman91@gmail.com>
2018-10-04 17:17:33 -0400

    javascript for the server

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vdn.api+json"}));
+
+app.listen(PORT, function() {
+    console.log("Listening on PORT " + PORT);
+});
+
+require('./app/routing/htmlRoutes.js')(app);
+
+require('./app/routing/apiRoutes.js')(app);
